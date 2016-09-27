f8c53cc77b4571840a3ac9afda8c01d5bce32588
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-09-27 16:51:02 +0300

    Add Product enumeration

---
+++ src/com/epam/pos/Product.java
@@ -0,0 +1,21 @@
+package com.epam.pos;
+
+public enum Product {
+    TEA(25, "Tea"), COFFEE(35, "Coffee"), JUICE(45, "Juice");
+
+    private int price;
+    private String description;
+
+    Product(int price, String description) {
+        this.price = price;
+        description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
